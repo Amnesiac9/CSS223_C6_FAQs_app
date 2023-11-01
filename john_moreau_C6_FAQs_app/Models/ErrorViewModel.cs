0e67e84130df3f38dc59e51fd6e118c6f334dadd
John Moreau <johnmoreau19@gmail.com>
2023-10-31 22:24:54 -0700

    Add project files.

---
+++ john_moreau_C6_FAQs_app/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace john_moreau_C6_FAQs_app.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
