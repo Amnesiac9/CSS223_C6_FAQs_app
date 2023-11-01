e173a9d170f1d28ac5eb8508e1987f2c3f9d81d1
John Moreau <johnmoreau19@gmail.com>
2023-10-31 23:01:15 -0700

    Initial lsit of FAQ page

---
+++ john_moreau_C6_FAQs_app/Models/FAQ.cs
@@ -0,0 +1,11 @@
+﻿namespace john_moreau_C6_FAQs_app.Models
+{
+    public class FAQ
+    {
+        public int Id { get; set; }
+        public string Question { get; set; } = string.Empty;
+        public string Answer { get; set; } = string.Empty;
+        public string Topic { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty;
+    }
+}
